9d26a17d73071133e2d1bd179dcfac4e98ab956e
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-09-12 11:19:24 -0400

    creation du modele logique pour la bd(a verifier)

--- Application/Client/src/main.jsx
+++ Application/Client/src/main.jsx
@@ -1,11 +1,11 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Accueil from "./Pages/Accueil/Accueil.jsx";
-import Inscription from "./Pages/Inscription/Inscription.jsx";
-import PageErreur from "./Pages/PageErreur/PageErreur.jsx";
-import Connexion from "./Pages/Connexion/Connexion.jsx";
-import DashBoard from "./Pages/DashBoard/DashBoard.jsx";
+import Accueil from "./Pages/accueil/Accueil.jsx";
+import Inscription from "./Pages/inscription/Inscription.jsx";
+import PageErreur from "./Pages/pageErreur/PageErreur.jsx";
+import Connexion from "./Pages/connexion/Connexion.jsx";
+import DashBoard from "./Pages/dashBoard/DashBoard.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
