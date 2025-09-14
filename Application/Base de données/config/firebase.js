82c69feed2eb1fa464093e88a15bda7a62c7ab21
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-09-14 16:26:37 -0400

    firebase log tool install

---
+++ Application/Base de données/config/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAubMliDmFlmeqvjOv-l-K2Ib4WpA5ncvs",
+  authDomain: "cavalier-project-a25.firebaseapp.com",
+  projectId: "cavalier-project-a25",
+  storageBucket: "cavalier-project-a25.firebasestorage.app",
+  messagingSenderId: "873309354426",
+  appId: "1:873309354426:web:e9456c3a90d64e1c12c583",
+  measurementId: "G-9VJBDFGHB3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
