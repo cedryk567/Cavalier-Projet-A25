4ff2bfccf7dc051001f74ac507122366e7eaa635
mmaanneell <150070696+mmaanneell@users.noreply.github.com>
2025-09-21 19:32:59 -0400

    Réglage de quelques détails d'UI sur page Connexion
    + import de polices Graduate et Koulen sur index.html

--- Application/Client/src/Pages/Connexion/Connexion.jsx
+++ Application/Client/src/Pages/Connexion/Connexion.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import logoCavaliers from '../../img/Logo_Noir.png';
-
+const classNameRandom = "d-flex flex-column align-items-center text-white";
 function Connexion() {
   return (
   
-    <div className="d-flex flex-column align-items-center text-white" style={{backgroundColor: "#0D0D0D", height: "100%", width: "100%"}}>
+    <div className="d-flex flex-column align-items-center text-white" style={{backgroundColor: "#0D0D0D", height: "100%", minHeight: "120vh", width: "100%"}}>
       
       {/* Container du formulaire */}
-      <div className="d-flex flex-column align-items-center" style={{paddingTop:"3rem", paddingBottom:"3rem", backgroundColor: "#1A1A1A", borderRadius: "1rem", margin: "6rem", gap: "1.5rem"}}>
+      <div className="d-flex flex-column align-items-center" style={{paddingTop:"2rem", paddingBottom:"2rem", backgroundColor: "#1A1A1A", borderRadius: "1rem", margin: "3rem", gap: "1rem", width: "40%"}}>
+        {/* <div className="d-flex flex-column align-items-center" style={{paddingTop:"3rem", paddingBottom:"3rem", backgroundColor: "#1A1A1A", borderRadius: "1rem", margin: "6rem", gap: "1.5rem", width: "40%"}}>*/}
         
-        <img src={logoCavaliers} style={{height: "10rem"}} />
-        <h1 style={{color:"#65C97A"}}> Connectez-Vous</h1>
-        <h2 style={{maxWidth: "70%", textAlign:"center"}}> Chaque connexion vous rapproche de la victoire. </h2>
+        <img src={logoCavaliers} style={{height: "7rem"}} />
+        <h1 style={{color:"#65C97A", fontFamily:"Koulen"}}> CONNECTEZ-VOUS</h1>
+        <h3 style={{maxWidth: "72%", textAlign:"center", fontFamily:"Graduate"}}>  CHAQUE CONNECTION VOUS RAPPROCHE DE LA VICTOIRE.  </h3>
 
-        <form className="d-flex flex-column align-items-center" style={{gap: "0.5rem", width: "70%"}}>
-          <input type="email" placeholder="Adresse courriel" required style={{backgroundColor:"rgba(0, 0, 0, 0.4)", borderColor:"#65C97A", borderRadius:"5px", width:"100%", height:"3rem"}}/>
-          <input type="password" placeholder="Mot de passe" required style={{backgroundColor:"rgba(0, 0, 0, 0.4)", borderColor:"#65C97A", borderRadius:"5px", width:"100%", height:"3rem"}}/>
+        <form className="d-flex flex-column align-items-center" style={{gap: "1rem", width: "70%", marginTop: "1rem"}}>
+          <input type="email" placeholder="Adresse courriel" required style={{backgroundColor:"rgba(0, 0, 0, 0.4)", border:"1.5px solid #65C97A", borderRadius:"8px", borderWidth:"medium", width:"100%", height:"3.5rem", color:"white", padding: "0 1rem"}}/>
+          <input type="password" placeholder="Mot de passe" required style={{backgroundColor:"rgba(0, 0, 0, 0.4)", border:"1.5px solid #65C97A", borderRadius:"8px", borderWidth:"medium", width:"100%", height:"3.5rem", color:"white", padding: "0 1rem"}}/>
           <p style={{color:"#65C97A"}} >Mot de passe oublié?</p>          
-          <button type="submit" style={{backgroundColor:"rgba(0, 0, 0, 0.4)", borderColor:"#65C97A", borderRadius:"15px", color: "white", height:"3rem"}}>Se connecter</button>
+          <button className="justify-content-center" type="submit" style={{backgroundColor:"rgba(0, 0, 0, 0.4)", border:"1.5px solid #65C97A", borderRadius:"25px", color: "white", height:"3rem", padding: "0.6rem 1.4rem"}}>Se connecter</button>
         </form>
 
       </div>
