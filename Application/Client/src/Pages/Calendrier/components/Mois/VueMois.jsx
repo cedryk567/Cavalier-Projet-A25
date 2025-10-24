1e43fdc6d3f910ad1b745912799f228a1ed0bc63
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-10-24 10:36:26 -0400

    ajustement de style du calendrier

--- Application/Client/src/Pages/Calendrier/components/Mois/VueMois.jsx
+++ Application/Client/src/Pages/Calendrier/components/Mois/VueMois.jsx
@@ -1,5 +1,6 @@
 // src/Pages/Calendrier/components/VueMois.jsx
 import React from "react";
+import "./VueMois.css";
 import { getDateCalendrier } from "../../getDateCalendrier";
 
 export const VueMois = ({ jourSelectionner, setJourSelectionner, events }) => {
@@ -29,47 +30,33 @@ export const VueMois = ({ jourSelectionner, setJourSelectionner, events }) => {
   };
 
   return (
-    <div>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(7, 1fr)",
-          gap: "5px",
-        }}
-      >
-        {/* En-têtes de jours */}
-        {["Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"].map(
-          (jour, index) => (
-            <div
-              key={index}
-              style={{ fontWeight: "bold", textAlign: "center" }}
-            >
-              {jour}
-            </div>
-          )
-        )}
-
-        {/* Jours du calendrier */}
-        {jours.map((jour, index) => (
-          <div
-            key={index}
-            style={{
-              padding: 10,
-              textAlign: "center",
-              backgroundColor: jour.estMoisCourant ? "#f0f0f0" : "black",
-              borderRadius: 4,
-              border: jour.estMoisCourant
-                ? "0px solid black"
-                : "1px solid #f0f0f0",
-              color: jour.estMoisCourant ? "black" : "#f0f0f0",
-              width: "5rem",
-              height: "7rem",
-            }}
-          >
-            {jour.date.day}
-          </div>
-        ))}
-      </div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(7, 1fr)",
+        gridTemplateRows: "repeat(6, 1fr)",
+        gap: "5px",
+        height: "95%",
+        boxSizing: "border-box",
+      }}
+    >
+      {/* Jours du mois */}
+      {jours.map((jour, index) => (
+        <div
+          key={`day-${index}`}
+          style={{
+            textAlign: "center",
+            backgroundColor: jour.estMoisCourant ? "#f0f0f0" : "#2b2b2b",
+            borderRadius: "4px",
+            color: jour.estMoisCourant ? "black" : "#999",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {jour.date.day}
+        </div>
+      ))}
     </div>
   );
 };
