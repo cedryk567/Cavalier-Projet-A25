cf929e5f36eba7ee0e253f7431e9baa587afcb75
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-10-27 10:45:21 -0400

    Update main.jsx

--- Application/Client/src/main.jsx
+++ Application/Client/src/main.jsx
@@ -4,9 +4,9 @@ import Accueil from "./Pages/Accueil/Accueil.jsx";
 import ActivationCompte from "./Pages/ActivationCompte/ActivationCompte.jsx";
 import PageErreur from "./Pages/pageErreur/PageErreur.jsx";
 import Connexion from "./Pages/Connexion/Connexion.jsx";
-import DashBoard from "./Pages/dashBoard/DashBoard.jsx";
+import {DashBoard} from "./Pages/DashBoard/DashBoard.jsx";
 import Equipes from "./Pages/Equipes/Equipes.jsx";
-import APropos from "./Pages/APropos/APropos.jsx"
+import APropos from "./Pages/APropos/APropos.jsx";
 import DemanderMotDePasseTemporaire from "./Pages/DemanderMotDePasseTemporaire/demanderMotDePasseTemporaire.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -28,6 +28,24 @@ const router = createBrowserRouter([
   {
     path: "/DashBoard",
     element: <DashBoard />,
+    children: [
+      {
+        path: "messages",
+        element: <Messages />,
+      },
+      {
+        path: "calendrier",
+        element: <Calendrier />,
+      },
+      {
+        path: "statistiques",
+        element: <Statistique />,
+      },
+      {
+        path: "documents",
+        element: <Document />,
+      },
+    ],
   },
   {
     path: "/Equipes",
