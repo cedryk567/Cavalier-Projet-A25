4be1decaefad471162da4815246178f0068ef4ca
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-11-06 14:15:59 -0500

    Début du style pour La page Error

--- Application/Client/src/Pages/404ErrorPage/ErrorPage.jsx
+++ Application/Client/src/Pages/404ErrorPage/ErrorPage.jsx
@@ -1,9 +1,28 @@
+import "./ErrorPage.css";
+import { NavLink } from "react-router-dom";
+import { StyledText } from "../../components/ComposantsMajeur/StyledComponents/Text.style";
+import { StyledButtonSimpleBorder } from "../../components/ComposantsMajeur/StyledComponents/ButtonDashboard.style";
+
 function ErrorPage() {
   return (
-    <>
-      <h2>allo error 404</h2>
-    </>
+    <div className="ErrorContaier">
+      <StyledText margin="10px 0">
+        <span>Erreur 404</span>
+      </StyledText>
+      <StyledText size="2rem" weight="800">
+        <span>Désolé! Aucune Page Trouvée</span>
+      </StyledText>
+      <StyledText margin=" 0  0 20px 0">
+        <span>
+          La page que vous cherchez n'existe pas. Cliquez sur le bouton pour
+          revenir à l'accueil.
+        </span>
+      </StyledText>
+      <StyledButtonSimpleBorder to="/" colorText="white">
+        <span>Accueil</span>
+      </StyledButtonSimpleBorder>
+    </div>
   );
 }
 
-export default ErrorPage
+export default ErrorPage;
