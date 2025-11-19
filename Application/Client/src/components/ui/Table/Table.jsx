766cefb624f5a81ef98b443c9b27b3349567166b
cedryk567 <2246462@bdeb.qc.ca>
2025-11-19 17:48:38 -0500

    réparation de la scrollbar pour la page admin

--- Application/Client/src/components/ui/Table/Table.jsx
+++ Application/Client/src/components/ui/Table/Table.jsx
@@ -17,46 +17,48 @@ const Table = ({
   }
   const rowKeys = Object.keys(tableAffiche[0]);
   return (
-    <table className="Table">
-      <thead className="Header">
-        <tr>
-          {rowKeys.slice(1).map((header, i) => (
-            <th key={i}>{header}</th>
-          ))}
-          <th>Modifier</th>
-          <th>Supprimer</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {tableAffiche.map((row, i) => (
-          <tr key={i}>
-            {rowKeys.slice(1).map((key, j) => (
-              <td key={j}>{row[key]}</td>
+    <div className="TableContainer">
+      <table className="Table">
+        <thead className="Header">
+          <tr>
+            {rowKeys.slice(1).map((header, i) => (
+              <th key={i}>{header}</th>
             ))}
-            <td>
-              <Button
-                style={"buttonTable"}
-                onClick={() => {
-                  console.log(row);
-                  setModalAffiche(true);
-                  setTableModifier(row);
-                  setFiltreBlurry();
-                }}
-                contenue={EditSvg()}
-              ></Button>
-            </td>
-            <td>
-              <Button
-                style={"buttonTable"}
-                onClick={() => {}}
-                contenue={DeleteSVG()}
-              ></Button>
-            </td>
+            <th>Modifier</th>
+            <th>Supprimer</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {tableAffiche.map((row, i) => (
+            <tr key={i}>
+              {rowKeys.slice(1).map((key, j) => (
+                <td key={j}>{row[key]}</td>
+              ))}
+              <td>
+                <Button
+                  style={"buttonTable"}
+                  onClick={() => {
+                    console.log(row);
+                    setModalAffiche(true);
+                    setTableModifier(row);
+                    setFiltreBlurry();
+                  }}
+                  contenue={EditSvg()}
+                ></Button>
+              </td>
+              <td>
+                <Button
+                  style={"buttonTable"}
+                  onClick={() => {}}
+                  contenue={DeleteSVG()}
+                ></Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
