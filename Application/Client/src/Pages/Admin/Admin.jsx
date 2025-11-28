a0e5224cd074f58bde82fa66f194f3721ea938ce
cedryk567 <150829578+cedryk567@users.noreply.github.com>
2025-11-28 12:34:28 -0500

    update pour le formulaire

--- Application/Client/src/Pages/Admin/Admin.jsx
+++ Application/Client/src/Pages/Admin/Admin.jsx
@@ -6,48 +6,43 @@ import TextArea from "../../components/ui/TextArea/TextArea.jsx";
 import Presentoire from "../../components/ui/Presentoire/Presentoire.jsx";
 import Button from "../../components/ui/Button/Button.jsx";
 import Modal from "../../components/ui/Modal/Modal.jsx";
+import { updateUtilisateur } from "../../server/api/routeAdmin.jsx";
 import "./Admin.css";
 const Admin = () => {
   const [users, setUsers] = useState([]);
   const test = [1, 2, 3];
-  const [recherche, setRecherche] = useState("");
   const [modalEstAffiche, setModalEstAffiche] = useState(false);
   const [tableModifier, setTableModifier] = useState({});
   const [filtreBlurry, setFiltreBlurry] = useState("");
   useEffect(() => {
     loaderUsers(setUsers);
-  }, []);
+  }, [modalEstAffiche]);
   return (
-      <div className="ContainerAdmin">
-        <Modal
-          style={"ModalAdmin"}
-          estActif={modalEstAffiche}
-          donneesElement={tableModifier}
-          setEstAffiche={setModalEstAffiche}
-          setFiltreBlurry={setFiltreBlurry}
-        />
-        <div className={`ContainerItems ${filtreBlurry}`}>
-          <Presentoire items={test} />
-          <div className="TextAreaButton">
-            <TextArea
-              textRecherche={recherche}
-              setTextRecherche={setRecherche}
-              style={"textAreaAdmin"}
-              placeHolder={"dddddddddd"}
-            />
-            <Button contenue={"Rechercher"} style={"buttonAdmin"} />
-          </div>
-          <Table
-            tableAffiche={users}
-            setModalAffiche={setModalEstAffiche}
-            setFiltreBlurry={() => {
-              setFiltreBlurry("ContainerItemsBlur");
-            }}
-            modalEstAffiche={modalEstAffiche}
-            setTableModifier={setTableModifier}
-          />
+    <div className="ContainerAdmin">
+      <Modal
+        style={"ModalAdmin"}
+        estActif={modalEstAffiche}
+        donneesElement={tableModifier}
+        setEstAffiche={setModalEstAffiche}
+        setFiltreBlurry={setFiltreBlurry}
+        enregistrerItem={updateUtilisateur}
+      />
+      <div className={`ContainerItems ${filtreBlurry}`}>
+        <Presentoire items={test} />
+        <div className="TextAreaButton">
+          <Button onClick={() => {setModalEstAffiche(true)}} contenue={"+ Ajouter"} style={"buttonAdmin"} />
         </div>
+        <Table
+          tableAffiche={users}
+          setModalAffiche={setModalEstAffiche}
+          setFiltreBlurry={() => {
+            setFiltreBlurry("ContainerItemsBlur");
+          }}
+          modalEstAffiche={modalEstAffiche}
+          setTableModifier={setTableModifier}
+        />
       </div>
+    </div>
   );
 };
 export default Admin;
